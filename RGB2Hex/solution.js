5a8f76a88a3d5501b049973ee805d3d5a64b2fb4
Steve <s.mattison@gmail.com>
2025-03-07 00:00:17 -0800

    Commented solution for RGB2Hex

--- RGB2Hex/solution.js
+++ RGB2Hex/solution.js
@@ -1,15 +1,19 @@
 function validate(n){
+  //Remove invalid values outside of range (0 through 255 ONLY)
   switch(true){
     case (n<0):
-      n=0;break;
+      n=0;break; //Too low!
     case (n>255):
-      n=255;
+      n=255; //Too high!
   }
+  //Convert the integers to hex strings, padding out with zeroes, uppercased
   return n.toString(16).padStart(2, '0').toUpperCase();
 }
 function rgb(r, g, b) {
+  //validate each color
 	r = validate(r);
 	g = validate(g);
 	b = validate(b);
+  //return the parsed color code
   return r + g + b;
 }
