b9118937bb0fcfb8854ed2895aba4ab22e330c99
Steve <s.mattison@gmail.com>
2025-03-11 23:25:57 -0700

    Solution for Count characters in your string

--- CountChars/README.md
+++ CountChars/README.md
@@ -1,12 +1,18 @@
 # Problem: Count characters in your string
 	Difficulty: 6
 	Date Completed: 2025/03/11
+
 ## 📜Instructions
 	Full problem description:
 	[CodeWars Link](https://www.codewars.com/kata/52efefcbcdf57161d4000091)
+
 ## 🛠Function(s) Usage:
-	Function: _;
+	Function: count(string);
+
 ### Inputs:
-	_
+	A string element
+
 ### Returns or Examples:
-    _
+    A variable length object with key:value pairs corresponding to the
+	character elements in the string.
+	"aba" returns {'a': 2, 'b': 1}
--- CountChars/solution.js
+++ CountChars/solution.js
@@ -1,8 +1,22 @@
 /*
   Solution for "Count characters in your string"
   CodeWars Link: https://www.codewars.com/kata/52efefcbcdf57161d4000091
+
   Complexity Analysis:
-  - Time Complexity: O(_) - _
-  - Space Complexity: O(_) - _
+  - Time Complexity: O(n) - Linear: Iterating through a string
+  - Space Complexity: O(k) - Linear-ish scaling with the # of unique keys
 */
-_
+
+function count(string) {
+	//create the object literal
+	let response = {};
+	//loop through the string
+	for(chr=0;chr<string.length;chr++){
+		//grab the current character
+		let key = string.charAt(chr);
+		//add it to the list and increment
+		response[key] = (response[key] || 0) + 1; 
+	}
+	//and return
+	return response;
+}
